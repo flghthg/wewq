3b8a594f102686ba3d3c6f931574aa0813328ed1
flghthg <flghthg12345@gmail.com>
2022-12-02 05:05:29 +0900

    first commit

---
+++ src/main/java/com/example/springbootofvscode/SpringBootOfVsCodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootofvscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOfVsCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOfVsCodeApplication.class, args);
+	}
+
+}
